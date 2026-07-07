60f2fcb1007f28ad507cad4cbe47a8c8f9dbdc65
suhanisharmaa100-max <suhanisharmaa100@gmail.com>
2026-07-08 02:07:45 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,14 @@
+//=============================================================VARIABLE IN JAVA =======================================================================
+package day01javaBasics;
+public class Variable {
+public static void main(String[] args) {
+ 
+    int age = 21;
+    String name = "suhani";
+    double salary = 50000.50;
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("salary: " + salary);
+    }
+}
