c08ce21cb67f5bfe2ae5dc99ae83eed5e5dc4c9e
suhanisharmaa100-max <suhanisharmaa100@gmail.com>
2026-07-08 23:26:45 +0530

    Add files via upload

---
+++ Copyvalue.java
@@ -0,0 +1,12 @@
+//======================================================COPY VALUE OF ANOTHER VARIABLE====================================================================
+package day02javaBasics;
+
+public class Copyvalue {
+     
+    public static void main(String[] args){
+    int a = 10;
+    int b = a;
+    System.out.println(b);
+    }
+}
+
---
+++ Datatype.java
@@ -0,0 +1,45 @@
+//====================================================================DATA TYPES IN JAVA=================================================
+
+
+//=======BOOLEAN DATATYPE=====
+
+// package day02javaBasics;
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//     boolean ishefunny = true;
+//     System.out.println(" Is he funny : " + ishefunny);
+//     }
+// }
+
+
+//=======BYTE DATATYPE ======
+
+// package day02javaBasics;
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//         byte age = 21;
+//         System.out.println("Age : " + age);
+
+//     }
+// }
+
+//======SHORT DATATYPE========
+// package day02javaBasics;
+
+// public class Datatype{
+//     public static void main(String[] args) {
+//         short students = 1000;
+//         System.out.println("Total number of students : " + students);
+//     }
+// }
+//======INTEGER DATATYPE=======
+package day02javaBasics;
+
+public class Datatype{
+    public static void main(String[] args) {
+        int population = 2000000;
+        System.out.println(population);
+    }
+}
